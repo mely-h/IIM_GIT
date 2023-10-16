277e7d9f55ebbf124b41e4ab3141562a1d289880
Kieran HUON <kieran.huon@gmail.com>
2023-10-16 17:51:39 +0100

    Finalisation de l'inscription. Fix #2

--- register.php
+++ register.php
@@ -1,10 +1,43 @@
-<?php session_start();
+
+
+<?php 
+session_start();
 require('config/config.php');
 require('model/functions.fn.php');
 
 /*===============================
-	Register
+    Register
 ===============================*/
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        // Vérification de la disponibilité de l'username et de l'email
+        if (isUsernameAvailable($db, $username)) {
+            if (isEmailAvailable($db, $email)) {
+                
+                
+                // Appel de la fonction d'inscription
+                if(userRegistration($db, $username, $email, $password)) {
+					
+					
+                    
+                } else {
+                    $error = "Erreur lors de l'inscription. Veuillez réessayer.";
+                }
+            } else {
+                $error= "Email indisponible";
+            }
+        } else {
+            $error = "Username indisponible";
+        }
+    } else {
+        $error = "Tous les champs sont obligatoires!";
+    }
+}
 
 include 'view/_header.php';
 include 'view/register.php';
